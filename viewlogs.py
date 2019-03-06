df94b3ab1fd9f2f3918a6973e31767f8940d7e39
jkase888 <jdotkase@gmail.com>
2019-03-06 12:48:06 -0500

    Revert "v5.31"
    This reverts commit be80371ec877eaaf4cf30d8a1bcc0334272ce33c.

--- viewlogs.py
+++ viewlogs.py
@@ -7,7 +7,6 @@ def view_logs():
     f = open("short_log.txt", "r")
     log_contents = f.read()
     f.close()
-    log_contents = log_contents.replace('\n', '<br>')
     return log_contents
 
 @app.route('/long')
