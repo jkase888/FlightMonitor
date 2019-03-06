4cd836929ad0feb972fc8ba6db8f1f07e59144b0
jkase888 <jdotkase@gmail.com>
2019-03-06 11:38:30 -0500

    Update viewlogs.py

--- viewlogs.py
+++ viewlogs.py
@@ -4,10 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def view_logs():
-    f = open("short_log.txt", "r")
-    log_contents = f.read()
-    f.close()
-    return "hello world"
+    return 'hello world!'
+    #f = open("short_log.txt", "r")
+    #log_contents = f.read()
+    #f.close()
+    #return log_contents
 
 if __name__ == '__main__':
     app.run()
