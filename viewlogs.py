807f7cd264371556e7847023acba85e555175500
jkase888 <jdotkase@gmail.com>
2019-03-06 12:52:37 -0500

    v5.5

--- viewlogs.py
+++ viewlogs.py
@@ -7,6 +7,7 @@ def view_logs():
     f = open("short_log.txt", "r")
     log_contents = f.read()
     f.close()
+    log_contents = log_contents.replace('\n', '<br>')
     return log_contents
 
 @app.route('/long')
