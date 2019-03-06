007ba285b39c3b6c8f0fe11caaf634a99d765dbe
jkase888 <jdotkase@gmail.com>
2019-03-06 12:46:00 -0500

    v5.4

--- viewlogs.py
+++ viewlogs.py
@@ -10,5 +10,13 @@ def view_logs():
     log_contents = log_contents.replace('\n', '<br>')
     return log_contents
 
+@app.route('/long')
+def view_logs():
+    f = open("raw_log.txt", "r")
+    log_contents = f.read()
+    f.close()
+    log_contents = log_contents.replace('\n', '<br>')
+    return log_contents
+
 if __name__ == '__main__':
     app.run()
