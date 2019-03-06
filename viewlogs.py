b333b86fdf24181dab6aaa0bbbcde74e004c9497
jkase888 <jdotkase@gmail.com>
2019-03-06 11:24:51 -0500

    Update viewlogs.py

--- viewlogs.py
+++ viewlogs.py
@@ -3,11 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def view_logs():
     f = open("short_log.txt", "r")
     log_contents = f.read()
     f.close()
     return log_contents
 
 if __name__ == '__main__':
-    app.run() 
+    app.run()
